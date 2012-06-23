e77db2e7e21fde867d092ffdc49c9cd077e14587
Tristan Blease <tblease@groupon.com>
2012-06-23 09:02:59 -0500

    auto commit

--- setup.sh
+++ setup.sh
@@ -1,34 +1,82 @@
 #!/bin/bash
 
-echo Creating symbolic links to dotfile assets...
+# ## ~/dotfiles/setup.sh
+#
+# Create symlinks to dotfiles provided by this repo
+#
+# Run at your own risk; this will delete existing dotfiles
+# in your home folder. There are no backups.
+#
+# Author: Tristan Blease <tristan@bleaseinteractive.com>
+# Website: tristanblease.com
+# Updated: 6/15/12
+#
 
-(
 
-# Go home
-cd ~
+# Break on errors
+set -e 
 
-# Set up vim folder
-[ -d .vim ] && rm -rf .vim
-ln -s dotfiles/vim .vim
 
-# Set up vimrc
-[ -e .vimrc ] && rm .vimrc
-ln -s dotfiles/vimrc .vimrc
+# Actual method
+function write-dot-files() {
 
-# Set up gvimrc
-[ -e .gvimrc ] && rm .gvimrc
-ln -s dotfiles/gvimrc .gvimrc
+  # Start a subshell so we don't leave the current directory 
+  # the script is started from
+  (
 
-# Set up zshrc
-[ -e .zshrc ] && rm .zshrc
-ln -s dotfiles/zshrc .zshrc
+    echo Creating symbolic links to dotfile assets...
 
-# Tmux configuration
-[ -e .tmux.conf ] && rm .tmux.conf
-ln -s dotfiles/tmux.conf .tmux.conf
+    # Clear our output
+    OUTPUT=''
 
-# dotjs - see http://defunkt.io/dotjs/
-[ -d .js ] && rm -rf .js
-ln -s dotfiles/js .js
+    # Go home
+    cd ~
+   
+    # Function to let us quickly replace files with a symbolic link
+    function replace-dotfile() {
+      [ -d $1 -o -e $1 -o -h $1 ] && rm -rf $1
+      ln -s $2 $1
+      OUTPUT="${OUTPUT}~/$1 -> ~/$2|"
+    }
+   
+    # Set up vim
+    replace-dotfile .vim dotfiles/vim
+    replace-dotfile .vimrc dotfiles/vim/vimrc
+    replace-dotfile .gvimrc dotfiles/vim/gvimrc
+   
+    # Set up zshrc
+    replace-dotfile .zshrc dotfiles/zsh/zshrc
+   
+    # Tmux configuration
+    replace-dotfile .tmux.conf dotfiles/tmux/tmux.conf
+   
+    # dotjs - see http://defunkt.io/dotjs/
+    replace-dotfile .js dotfiles/js
+    
+    # Show the results of our operation
+    echo Updated the following files:
+    echo $OUTPUT | tr "|" "\n" | column -t -s ' '
+    echo 
+   
+    echo ...finished
+  )
+   
+  exit 0
 
-)
+}
+
+# Make sure we want to continue...
+echo "WARNING! This script will destroy any existing dotfiles in your" 
+read -r -p "home folder; are you sure you want to continue? [y/n] " response
+case $response in
+    [yY][eE][sS]|[yY])
+        write-dot-files
+        ;;
+
+    *)
+	echo -e "\n...aborted"
+        exit 1
+        ;;
+    
+esac
+
